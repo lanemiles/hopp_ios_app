6bdabb9c95e70642c81eb21e86d333cae212f946
Lane Miles <lanem1234@aol.com>
2015-02-11 13:58:34 -0800

    Initial Commit

---
+++ Hopp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hopp
+//
+//  Created by Lane Miles on 2/11/15.
+//  Copyright (c) 2015 Lane Miles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hopp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hopp
+//
+//  Created by Lane Miles on 2/11/15.
+//  Copyright (c) 2015 Lane Miles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
