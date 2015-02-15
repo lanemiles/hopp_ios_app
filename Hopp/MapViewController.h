f227a6da4792c47228ec84ebfcfcb48c7a297346
Lane Miles <lanem1234@aol.com>
2015-02-14 23:09:40 -0800

    Create map VC

---
+++ Hopp/MapViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapViewController.h
+//  Hopp
+//
+//  Created by Lane Miles on 2/14/15.
+//  Copyright (c) 2015 Lane Miles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewController : UIViewController
+
+@end
